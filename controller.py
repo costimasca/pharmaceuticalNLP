b5b7b95ac012cf0d707026b9e65843ef513568df
costimasca <costimasca@gmail.com>
2018-05-05 23:43:02 +0300

    updated GUI

--- controller.py
+++ controller.py
@@ -10,14 +10,12 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super(ExampleApp, self).__init__(parent)
         self.setupUi(self)
         self.pushButton.clicked.connect(self.parse)
+        self.pushButton_2.clicked.connect(self.load_text)
         self.model = model.Model()
         self.css = '''
         label {
             font-style: normal;
             padding-right: 4 px;
-        }
-        body {
-            background-color: gray;
         }
         '''
         self.text_doc = QtGui.QTextDocument()
@@ -28,33 +26,57 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             'O': '#FFFFFF',
             'DOS': '#EE964B',
             'UNIT': '#F95738',
-            'WHO': '#FAF0CA',
-            'FREQ': '#F4D35E',
-            'PER': '#1D7AD1'
+            'WHO': '#8F78AD',
+            'FREQ': '#D4BA6A',
+            'PER': '#BBCD67'
         }
+
+        self.label.setText(f"<html><head/><body><p><span style=\" background-color:"
+                           f"{self.color_dict['DOS']};\">Dosage</span></p></body></html>")
+        self.label_2.setText(f"<html><head/><body><p><span style=\" background-color:"
+                             f"{self.color_dict['UNIT']};\">Unit</span></p></body></html>")
+        self.label_3.setText(f"<html><head/><body><p><span style=\" background-color:"
+                             f"{self.color_dict['WHO']};\">Who</span></p></body></html>")
+        self.label_4.setText(f"<html><head/><body><p><span style=\" background-color:"
+                             f"{self.color_dict['FREQ']};\">Frequency</span></p></body></html>")
+        self.label_5.setText(f"<html><head/><body><p><span style=\" background-color:"
+                             f"{self.color_dict['PER']};\">Period</span></p></body></html>")
+
+    def load_text(self):
+        fname = QtWidgets.QFileDialog.getOpenFileName(QtWidgets.QMainWindow(), 'Open file', '~', "All files *")
+
+        with open(fname[0]) as f:
+            text = f.read(-1)
+            f.close()
+
+        self.text_doc.setHtml('<body>' + text + '</body>')
 
     def parse(self):
         text = self.textEdit.toPlainText()
 
         parsed_sentences = self.model.predict(text)
         self.textEdit.clear()
+
+        labeled_text = []
         for sentence in parsed_sentences:
-            self.textEdit.setDocument(self.__get_rich_text(sentence))
+            labeled_text += sentence
+
+        self.textEdit.setDocument(self.__get_rich_text(labeled_text))
 
     def __get_rich_text(self, parsed_text):
-        rich_text = f'<body><nobr>'
+        rich_text = f'<body>'
         length = len(parsed_text)
         for i, (word, label) in enumerate(parsed_text):
             if label == 'O':
-                rich_text += f'<span>{word}</span>'
+                rich_text += f'<label>{word}</label>'
             else:
-                rich_text += f'<span style=\"background-color:{self.color_dict[label]}\">{word}</span>'
+                rich_text += f'<nobr><label style=\"background-color:{self.color_dict[label]}\">{word}</label></nobr>'
 
             # to keep the full stop next to the last word
             if i < length - 2:
-                rich_text += f"<span> </span>"
+                rich_text += f"<label> </label>"
 
-        rich_text += f'</nobr></body>'
+        rich_text += f'</body>'
         self.text_doc.setHtml(rich_text)
         return self.text_doc
 
